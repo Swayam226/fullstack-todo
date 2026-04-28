70f735d2df484a41ba9c7a03efabe87373cea2c4
Swayam <swayamkumar226@gmail.com>
2026-04-28 21:09:52 +0000

    get from frontend works, tasks visible

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   const [tasks, setTasks] = useState([]);
   const [currentTask, setcurrentTask] = useState("");
 
-  function handleClick() {
+  function createTask() {
     console.log("clicked");
   }
 
@@ -37,17 +37,40 @@ function App() {
             className="w-full mb-6 p-2 border rounded-lg"
           />
           <button
-            onClick={handleClick}
+            onClick={createTask}
             className="w-full mb-12 p-2 border rounded-lg bg-[#d9d7d7] hover:bg-[#cac9c9] transition duration-150 ease-in"
           >
             Create Task
           </button>
 
-          <div className="p-7 w-full bg-red-300 rounded-lg">
+          <div className="p-3 w-full bg-red-300 rounded-lg flex flex-col gap-4">
             {tasks.length === 0 ? (
               <span className="text-red-500">No Tasks Found :/</span>
             ) : (
-              <div>helo</div>
+              tasks.map((task) => {
+                return (
+                  <div className="p-4 w-full h-full bg-[rgb(245,245,220)] flex flex-row gap-2 justify-between">
+                    <div className="flex flex-col gap-2">
+                      <span className="text-xl font-semibold">
+                        {task.title}
+                      </span>
+                      {task.isCompleted ? (
+                        <span className="text-green-500">Done</span>
+                      ) : (
+                        <span className="text-red-500 text-xs">Pending</span>
+                      )}
+                    </div>
+                    <div className="flex flex-col gap-2">
+                      <button className="border rounded-sm text-xs p-1">
+                        Mark as Done
+                      </button>
+                      <button className="border rounded-sm text-xs p-1">
+                        Delete
+                      </button>
+                    </div>
+                  </div>
+                );
+              })
             )}
           </div>
         </div>
