cd80c8c5a9cfc6b3ead0971eb01d87b446699757
Swayam <swayamkumar226@gmail.com>
2026-05-06 19:25:39 +0000

    created dirs, implementing auth

--- backend/index.js
+++ backend/index.js
@@ -1,90 +1,85 @@
 const express = require('express');
-const crypto = require('crypto');
+// const crypto = require('crypto');
 const cors = require('cors');
-const fs = require('fs');
+// const fs = require('fs');
+
+const authRoutes = require("./routes/auth");
+const taskRoutes = require("./routes/tasks");
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.get('/tasks', function (req, res) {
-    fs.readFile('todos.json', 'utf-8', function (err, data) {
-        if (err) {
-            res.status(404).json({ "message": "Data not found" });
-            return;
-        }
-        const todos = JSON.parse(data);
-        res.status(200).json(todos);
-    })
-})
+app.use('/auth', authRoutes);
+
+// app.get('/tasks', function (req, res) {
+//     fs.readFile('todos.json', 'utf-8', function (err, data) {
+//         if (err) {
+//             res.status(404).json({ "message": "Data not found" });
+//             return;
+//         }
+//         const todos = JSON.parse(data);
+//         res.status(200).json(todos);
+//     })
+// })
 
 
-app.post('/tasks', function (req, res) {
-    fs.readFile("todos.json", 'utf-8', function (err, data) {
-        if (err) {
-            res.status(404).json({ "message": "Data not found" });
-            return;
-        }
-        const todos = JSON.parse(data);
-        let isCompleted = false;
-        const title = req.body.title;
-        const todo = {
-            id: crypto.randomUUID(),
-            title: title,
-            isCompleted: isCompleted
-        }
-        todos.push(todo);
-        fs.writeFileSync("todos.json", JSON.stringify(todos, null, 2));
-        res.status(201).json(todo);
-    })
-})
+// app.post('/tasks', function (req, res) {
+//     fs.readFile("todos.json", 'utf-8', function (err, data) {
+//         if (err) {
+//             res.status(404).json({ "message": "Data not found" });
+//             return;
+//         }
+//         const todos = JSON.parse(data);
+//         let isCompleted = false;
+//         const title = req.body.title;
+//         const todo = {
+//             id: crypto.randomUUID(),
+//             title: title,
+//             isCompleted: isCompleted
+//         }
+//         todos.push(todo);
+//         fs.writeFileSync("todos.json", JSON.stringify(todos, null, 2));
+//         res.status(201).json(todo);
+//     })
+// })
 
 
-app.patch('/tasks/:id', function (req, res) {
-    fs.readFile("todos.json", "utf-8", function (err, data) {
-        if (err) {
-            res.status(404).json({ "message": "Data not found" });
-            return;
-        }
-        const todos = JSON.parse(data);
-        const id = req.params.id;
-        const index = todos.findIndex(todo => todo.id === id);
-        if (index == -1) {
-            res.status(404).json({ message: "Todo Not Found" });
-            return;
-        }
-        todos[index].isCompleted = !todos[index].isCompleted;
-        fs.writeFileSync('todos.json', JSON.stringify(todos, null, 2));
-        res.status(200).json(todos[index]);
-    })
-})
+// app.patch('/tasks/:id', function (req, res) {
+//     fs.readFile("todos.json", "utf-8", function (err, data) {
+//         if (err) {
+//             res.status(404).json({ "message": "Data not found" });
+//             return;
+//         }
+//         const todos = JSON.parse(data);
+//         const id = req.params.id;
+//         const index = todos.findIndex(todo => todo.id === id);
+//         if (index == -1) {
+//             res.status(404).json({ message: "Todo Not Found" });
+//             return;
+//         }
+//         todos[index].isCompleted = !todos[index].isCompleted;
+//         fs.writeFileSync('todos.json', JSON.stringify(todos, null, 2));
+//         res.status(200).json(todos[index]);
+//     })
+// })
 
 
-app.delete('/tasks/:id', function (req, res) {
-    fs.readFile("todos.json", "utf-8", function (err, data) {
-        if (err) {
-            res.status(404).json({ message: "Data not found" });
-            return;
-        }
-        const todos = JSON.parse(data);
-        const id = req.params.id;
-        const index = todos.findIndex(todo => todo.id === id);
-        const temp = todos[index];
-        todos.splice(index, 1);
-        fs.writeFileSync("todos.json", JSON.stringify(todos, null, 2));
-        res.status(200).json(temp)
-    })
-})
-
-
-
-
-
-
-
-
-
-
+// app.delete('/tasks/:id', function (req, res) {
+//     fs.readFile("todos.json", "utf-8", function (err, data) {
+//         if (err) {
+//             res.status(404).json({ message: "Data not found" });
+//             return;
+//         }
+//         const todos = JSON.parse(data);
+//         const id = req.params.id;
+//         const index = todos.findIndex(todo => todo.id === id);
+//         const temp = todos[index];
+//         todos.splice(index, 1);
+//         fs.writeFileSync("todos.json", JSON.stringify(todos, null, 2));
+//         res.status(200).json(temp)
+//     })
+// })
 
 
 app.listen(3000)
---
+++ backend/routes/auth.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const router = express.Router();
+const fs = require("fs");
+const jwt = require("jsonwebtoken");
+
+
+router.post('/signup', function (req, res) {
+    const username = req.body.username;
+    const password = req.body.password;
+    fs.readFile("./data/users.json", 'utf-8', function (err, data) {
+        if (err) {
+            console.error(err)
+            res.status(403).json({ message: "data not found" });
+            return;
+        } else {
+            const userInfo = {
+                username,
+                password
+            }
+            const users = JSON.parse(data);
+            const existingUser = users.find((user) => user.username === username)
+            if (existingUser) {
+                res.status(409).json({
+                    message: "user already exists"
+                })
+                return;
+            }
+            users.push(userInfo);
+            fs.writeFileSync("./data/users.json", JSON.stringify(users, null, 2));
+            res.status(201).json(userInfo);
+        }
+    })
+})
+
+router.post('/signin', function (req, res) {
+    const username = req.body.username;
+    const password = req.body.password;
+    fs.readFile("./data/users.json", "utf-8", function (err, data) {
+        if (err) {
+            console.error(err);
+            res.status(403).json({ message: "data not found" });
+        } else {
+            const users = JSON.parse(data);
+            const existingUser = users.find((user) => user.username === username && user.password === password);
+            if (existingUser) {
+                const token = jwt.sign({
+                    username: username
+                }, process.env.JWT_SECRET)
+                res.json({ token: token });
+            } else {
+                res.status(403).json({ message: "entry declined" });
+            }
+        }
+    })
+})
+
+module.exports = router;
